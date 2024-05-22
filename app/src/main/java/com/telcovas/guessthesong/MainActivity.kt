b455b5118a10166d34965779ecee9481f48dfdb9
dheeraj92 <dheeraj.varshney@telcovasworld.com>
2024-05-22 19:51:49 +0530

    Purchase Packs UI Design

--- app/src/main/java/com/telcovas/guessthesong/MainActivity.kt
+++ app/src/main/java/com/telcovas/guessthesong/MainActivity.kt
@@ -5,10 +5,16 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
@@ -138,8 +144,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = stringResource(id = R.string.app_name),
+        modifier = modifier.fillMaxSize(),
+        color = colorResource(id = R.color.body_bg),
+        fontFamily = (FontFamily(Font(R.font.kavoon))),
+        fontSize = 18.sp
     )
 }
 
---
+++ app/src/main/java/com/telcovas/guessthesong/purchasePacks/PurchasePacksActivity.kt
@@ -0,0 +1,12 @@
+package com.telcovas.guessthesong.purchasePacks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.telcovas.guessthesong.R
+
+class PurchasePacksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase_packs)
+    }
+}
