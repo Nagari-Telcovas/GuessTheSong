2f6cf6e8176ceabd785421be840527613698fadd
dheeraj92 <dheeraj.varshney@telcovasworld.com>
2024-06-03 15:30:26 +0530

    Update UI design

---
+++ app/src/main/java/com/telcovas/guessthesong/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.telcovas.guessthesong.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.telcovas.guessthesong.MainActivity
+import com.telcovas.guessthesong.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        displaySplash()
+    }
+
+    private fun displaySplash() {
+        Handler(Looper.getMainLooper()).postDelayed({
+                val iLogin = Intent(this, MainActivity::class.java)
+                startActivity(iLogin)
+                finish()
+        }, 2000)
+    }
+}
