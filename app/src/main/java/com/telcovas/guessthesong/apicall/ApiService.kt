e93b47755ac8755330b3b6701a4f0442161efd54
Nagari-Telcovas <nagari.saidulu@telocovasworld.com>
2024-06-21 14:53:23 +0530

    updating with api calls

--- app/src/main/java/com/telcovas/guessthesong/apicall/ApiService.kt
+++ app/src/main/java/com/telcovas/guessthesong/apicall/ApiService.kt
@@ -2,6 +2,8 @@ package com.telcovas.guessthesong.apicall
 
 import com.telcovas.guessthesong.dashboard.QuizList
 import com.telcovas.guessthesong.leaderBoard.LeaderBoardList
+import com.telcovas.guessthesong.login.LoginResponse
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,7 +13,7 @@ interface ApiService {
     @GET("SongQuestionList?")
     suspend fun getSongQuestionList(@Query("reportType") reportType: String): List<QuizList>
 
-    @GET("SongQuestionList?")
+    @GET("LoginServlet?")
     suspend fun getLeaderBoardList(@Query("reportType") reportType: String): List<LeaderBoardList>
 
     @GET("LoginServlet?")
@@ -20,9 +22,13 @@ interface ApiService {
     @GET("LoginServlet?")
     suspend fun getTotalPlayers(@Query("reportType") reportType: String): List<QuizList>
 
+    @GET("LoginServlet?")
+    suspend fun getLogin(@Query("reportType") reportType: String,@Query("msisdn") msisdn: String,@Query("password") password: String): LoginResponse
+
 
 
 /*
+https://globicall.globicallservices.com/QuizIntegration/LoginServlet?reportType=userLogin&msisdn=${encodeURIComponent(msisdn)} LOGIN
     1..https://globicall.globicallservices.com/SongsQuizApp/SongQuestionList?reportType=questionList                     (QUESTIONS LIST)
 
     2...https://globicall.globicallservices.com/SongsQuizApp/SongQuestionList?reportType=InsertingUserDetails&
