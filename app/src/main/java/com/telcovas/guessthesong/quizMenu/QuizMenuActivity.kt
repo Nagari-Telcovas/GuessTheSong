df554784944655ec3dba0ae701259321491df7bc
dheeraj92 <dheeraj.varshney@telcovasworld.com>
2024-06-26 14:34:29 +0530

    Bug Fixes and Add quiz rules

--- app/src/main/java/com/telcovas/guessthesong/quizMenu/QuizMenuActivity.kt
+++ app/src/main/java/com/telcovas/guessthesong/quizMenu/QuizMenuActivity.kt
@@ -3,17 +3,17 @@ package com.telcovas.guessthesong.quizMenu
 
 import android.content.Intent
 import com.telcovas.guessthesong.BaseActivity
-import com.telcovas.guessthesong.dashboard.MainActivity
 import com.telcovas.guessthesong.R
 import com.telcovas.guessthesong.databinding.ActivityQuizMenuBinding
 import com.telcovas.guessthesong.leaderBoard.LeaderBoardActivity
 import com.telcovas.guessthesong.myWines.MyWinsActivity
+import com.telcovas.guessthesong.quizRules.QuizRulesActivity
 
 class QuizMenuActivity : BaseActivity<ActivityQuizMenuBinding>(ActivityQuizMenuBinding::inflate, R.string.app_name) {
 
     override fun initialization(bindingScreen: ActivityQuizMenuBinding) {
         bindingScreen.quizes.setOnClickListener {
-            var inItem = Intent(this, MainActivity::class.java)
+            var inItem = Intent(this, QuizRulesActivity::class.java)
             startActivity(inItem)
         }
 
---
+++ app/src/main/java/com/telcovas/guessthesong/quizRules/QuizRulesActivity.kt
@@ -0,0 +1,18 @@
+package com.telcovas.guessthesong.quizRules
+
+
+import android.content.Intent
+import com.telcovas.guessthesong.BaseActivity
+import com.telcovas.guessthesong.R
+import com.telcovas.guessthesong.dashboard.MainActivity
+import com.telcovas.guessthesong.databinding.ActivityQuizRulesBinding
+
+class QuizRulesActivity : BaseActivity<ActivityQuizRulesBinding>(ActivityQuizRulesBinding::inflate, R.string.quizRule) {
+
+    override fun initialization(bindingScreen: ActivityQuizRulesBinding) {
+        bindingScreen.continueQuiz.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
